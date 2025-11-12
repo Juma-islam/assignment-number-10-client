57017f9da0b18252f8ccafc594f11c1e65cdd507
Juma-islam <ononnaislamj@gmail.com>
2025-11-12 19:38:22 +0600

    added firebase config hide function

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,15 @@
-
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgeIrD0u0yP-6iJ7jr8IEq70qHMUtg6Vo",
-  authDomain: "community-cleanliness-project.firebaseapp.com",
-  projectId: "community-cleanliness-project",
-  storageBucket: "community-cleanliness-project.firebasestorage.app",
-  messagingSenderId: "996168780941",
-  appId: "1:996168780941:web:516acf389627d1acfa66c4"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
